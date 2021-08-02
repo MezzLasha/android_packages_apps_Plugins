e222c4f9834380ae5554cfb01d56471c7d47322d
mezzlasha <mezzlasha@gmail.com>
2021-08-02 18:23:32 +0000

    force white color always

--- ShapeShiftPanel/src/co/potatoproject/plugin/volume/ssos/VolumeDialogImpl.java
+++ ShapeShiftPanel/src/co/potatoproject/plugin/volume/ssos/VolumeDialogImpl.java
@@ -535,7 +535,7 @@ public class VolumeDialogImpl implements VolumeDialog {
         row.icon.setImageDrawable(iconResDrawable);
         row.icon.setImportantForAccessibility(View.IMPORTANT_FOR_ACCESSIBILITY_NO);
         row.icon.setClickable(false);
-        ColorStateList mIconTint = ColorStateList.valueOf(mContext.getResources().getColor(R.color.row_icon_dark));
+        ColorStateList mIconTint = ColorStateList.valueOf(mContext.getResources().getColor(R.color.row_icon_white));
         ColorStateList mIconTintNormal = ColorStateList.valueOf(mContext.getResources().getColor(R.color.row_icon_white));
         int rowprogress = row.slider.getProgress();
         if (rowprogress < 500) {
